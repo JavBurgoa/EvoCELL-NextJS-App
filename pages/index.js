cd8c18828fbd1c12dad08dc526223fb06b45323a
JavBurgoa <javierburgoac@gmail.com>
2021-05-21 15:00:47 +0200

    Added automatic table

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,55 @@ import path from 'path';
 
 const Home = ({ data }) => {
 	// List all JSON files given our structure
-
+	const columns = ['Species', 'Paper', 'Ontogenic_Stage', 'Number_of_cells', 'GEO_Number']
 	return (
+	<>
+		<h1>MetaData</h1>
+		<table id = "table_id" class = "Table">
+			<tr>
+				<th style = {{width:'15%'}}>Species</th>
+				<th>Paper</th>
+				<th>Ontogenic stage</th>
+				<th>Number of cells</th>
+				<th>GEO Number</th>
+			</tr>
+			{
+			data.map( array => <>
+							   <tr>
+									{columns.map( col => <td>{array[col]}</td>) }
+							   </tr>
+							   </>
+					)
+			}
+		</table>
+			<p>
+		  ############## This part below is just to show better how the optimized version works #####
+			 </p>
+		<table>
+			{
+				data.map( array => <>
+									 <tr>
+									 	<td>{array.Species}</td>
+										<td>{array.Paper}</td>
+										<td>{array.Ontogenic_Stage}</td>
+										<td>{array.Number_of_cells}</td>
+										<td>{array.GEO_Number}</td>
+									 </tr>
+								   </>
+							 )
+			}
+
+		</table>
+		##############
 		<div>
-			<h1>MetaData</h1>
+			
 			<p>
 			  {
 				data[1].Species
 			  }
 			  </p>
-			<p>
-			  {
-				data.map( file => <p key={file.Species}> {file.Species} </p>)
-			  }
-			  ##########################
-			</p>
 		</div>
+	</>
 	)
 };
 
